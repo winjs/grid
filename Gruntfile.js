d90a7e2407b4e506f63094f202c89c216c8b917f
Kyle Zinter <kylezinter@live.com>
2014-11-16 18:30:30 -0800

    Finishing off mixed direction language support
    Signed-off-by: Kyle Zinter <kylezinter@live.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,7 @@ module.exports = function (grunt) {
         sass: {
             dist: {
                 options: {
-                    style: 'expanded',
-                    sourcemap: true
+                    style: 'expanded'
                 },
                 files: [{
                     expand: true,
