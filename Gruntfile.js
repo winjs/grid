be29a8168f704675ee20725f7b6865a84886157f
Kyle Zinter <kylezinter@live.com>
2014-11-19 16:17:30 -0800

    Demo only - some clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,11 +25,23 @@ module.exports = function (grunt) {
             }
         },
 
+        autoprefixer: {
+            options: {
+                map: false
+            },
+            dist: {
+                files: {
+                    'css/demo/demo.css': 'css/demo/demo.css',
+                    'css/demo/msdotcomoffsetmenu.css': 'css/demo/msdotcomoffsetmenu.css'
+                }
+            }
+        },
+
         // Watch for changes
         watch: {
             sass: {
                 files: ['css/*.scss', 'css/demo/*.scss'],
-                tasks: ['sass']
+                tasks: ['sass', 'autoprefixer']
             },
             html: {
                 files: '*.html'
@@ -52,8 +64,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
 
     // Set Grunt tasks
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass', 'autoprefixer']);
     grunt.registerTask('server', ['connect', 'watch']);
 }
